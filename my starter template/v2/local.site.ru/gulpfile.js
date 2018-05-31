e8f8d2374c1b23c9452816ea17286bc422acc258
Ilfar Khuzin <ilfarhuzin@gmail.com>
2018-05-31 12:19:40 +0300

    V2.1 Starter Template
    Решил перейти на синтаксис scss

--- my starter template/v2/local.site.ru/gulpfile.js
+++ my starter template/v2/local.site.ru/gulpfile.js
@@ -9,13 +9,13 @@ gulp.task('serve', ['sass'], function() {
         server: "./app"
     });
 
-    gulp.watch("app/sass/*.sass", ['sass']);
+    gulp.watch("app/scss/*.scss", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/sass/*.sass")
+    return gulp.src("app/scss/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
